7c412cf82529372e5d74ac515bbf6f56411e236f
Vojtěch Struhár <vojtechstruhar@gmail.com>
2021-06-29 18:05:08 +0200

    Added basic footer.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2 className="App-header">{"---{Review Template}---"}</h2>
+        <h2 className="App-header">{"---{ Review Template }---"}</h2>
         <p className="App-header">☑ Awesome</p>
       </header>
+
+      <body>
+        <p>Body WIP :D</p>
+      </body>
+
+      <footer className="App-footer">
+        <p>Vojtěch Struhár, 2021</p>
+      </footer>
     </div>
   );
 }

ec90acb8935627518f598415ade29f0dbf8a718a
Vojtěch Struhár <vojtechstruhar@gmail.com>
2021-06-29 17:15:28 +0200

    Edited the page so its a little custom.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Vojta Struhár je borec.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"

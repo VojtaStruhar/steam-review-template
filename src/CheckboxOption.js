b08ceac7b022c3c045da2512d9b836bb8ff99ecc
Vojtěch Struhár <vojtechstruhar@gmail.com>
2021-06-30 18:10:18 +0200

    Checkbox options in place.

--- src/CheckboxOption.js
+++ src/CheckboxOption.js
@@ -3,25 +3,38 @@ import React, { useState } from 'react';
 import { Checkbox, FormControlLabel } from '@material-ui/core';
 
 
-function MyCheckbox() {
+function MyCheckbox(props) {
+    const [isChecked, setIsChecked] = useState(false)
+
     return (
         <FormControlLabel
-                // value="top"
-                control={<Checkbox color="primary" onClick={() => {console.log("click")} } />}
-                label={"Label"}
+                value={props.index}
+                control={<Checkbox color="primary" 
+                                   onClick={() => { setIsChecked(!isChecked) }} />}
+                label={props.title}
                 labelPlacement="end"
-                checked={false}
+                checked={isChecked}
             />
     );
 }
 
-export default function CheckboxOption() {
+export default function CheckboxOption(props) {
+    props = props.props
 
-    
+    const checkboxes = () => {
+        var array = []
+        for (let i = 0; i < props.options.length; i++) {
+            const element = props.options[i];
+            console.log(element)
+            array.push(<MyCheckbox title={element} index={i}/>)
+        }
+        return array
+    }
 
     return (
-        <div>
-            <p>CheckboxOption</p>
+        <div className="radio-container">
+            <h3 className="snug">{props.title}</h3>
+            {checkboxes()}
         </div>
     );
 

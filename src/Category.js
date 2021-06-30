578c56142d94ace7ef104df4c4d7b652309369b3
Vojtěch Struhár <vojtechstruhar@gmail.com>
2021-06-30 12:25:07 +0200

    Radio category centered.

--- src/Category.js
+++ src/Category.js
@@ -1,15 +1,17 @@
 import './Category.css';
 import React from 'react';
 import CheckboxOption from './CheckboxOption';
+import RadioOption from './RadioOption';
 
 export default class Category extends React.Component {
     render() {
         return (
             <div>
                 <div className="centered">
-                    <div className="section">
+                    <div>
                         <p>Paragraph</p>
                         <CheckboxOption description="description custom" />
+                        <RadioOption />
                     </div>
                 </div>
             </div>

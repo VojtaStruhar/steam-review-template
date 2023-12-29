d2ed3972614c8e6ae7b50055e9064069160a0bac
Vojtěch Struhár <vojtechstruhar@gmail.com>
2023-12-29 16:19:41 +0100

    Added github deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://vojtastruhar.github.io/",
+  base: "/steam-review-template",
+  integrations: [tailwind()],
 });

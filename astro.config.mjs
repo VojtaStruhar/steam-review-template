95c1de756f8eaf330081dee7b789b7408cd57e6d
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-01-09 13:27:35 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vojtastruhar.github.io/",
   base: "/steam-review-template",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
